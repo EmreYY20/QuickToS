8fa030301255fb11e660a40f27c294aa98fdb0c7
Emre <120115560+EmreYY20@users.noreply.github.com>
2024-07-24 14:27:38 +0200

    Update background.js

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,7 +3,7 @@ async function query(data) {
 	const response = await fetch(
 		"https://api-inference.huggingface.co/models/EE21/BART-ToSSimplify",
 		{
-			headers: { Authorization: "Bearer YOUR_HUGGINFACEKEY" },
+			headers: { Authorization: "Bearer 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" },
 			method: "POST",
 			body: JSON.stringify(data),
 		}
