7fe6e4412322f364d7d937c15b8672a6fe0681eb
Emre <120115560+EmreYY20@users.noreply.github.com>
2024-07-24 14:27:09 +0200

    Update popup.js

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -6,7 +6,7 @@ document.getElementById('summarize').addEventListener('click', async () => {
     const response = await fetch('https://api-inference.huggingface.co/models/EE21/BART-ToSSimplify', {
       method: 'POST',
       headers: {
-        'Authorization': 'Bearer YOUR_HUGGINFACEKEY',
+        'Authorization': 'Bearer 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',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ inputs: inputText })
